f0b845dae0bc9ac9ad8e614379c191ff10c1011f
Menaka Godakanda <156875412+MenakaGodakanda@users.noreply.github.com>
2024-07-25 17:10:11 +0800

    Update incident_response.py

--- respond/incident_response.py
+++ respond/incident_response.py
@@ -5,10 +5,14 @@ def isolate_machine(ip_address):
     subprocess.run(['sudo', 'iptables', '-A', 'OUTPUT', '-d', ip_address, '-j', 'DROP'])
 
 if __name__ == "__main__":
-    ip_address = '192.168.1.9'    # IP address of affected machine
+    ip_address = '192.168.1.4'    # IP address of affected machine
     isolate_machine(ip_address)
     print(f'Machine {ip_address} isolated')
 
     ip_address = '192.168.1.7'    # IP address of affected machine
+    isolate_machine(ip_address)
+    print(f'Machine {ip_address} isolated')
+    
+    ip_address = '192.168.1.9'    # IP address of affected machine
     isolate_machine(ip_address)
     print(f'Machine {ip_address} isolated')
